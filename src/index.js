ae1a12b41735b5c141296ffbb43f42c009b7df8a
Samuel <smarouba2@gmail.com>
2022-09-23 16:58:00 -0400

    commit tree bfe56e286b6fdab887de877462965ff9db0d46df

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', () => {
+  const breedDropDown = document.getElementById('breed-dropdown')
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+const dogBreeds = document.getElementById('dog-breeds')
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  const imageContainer = document.getElementById('dog-image-container')
+  fetch(imgUrl).then(res => res.json()).then(data => {
+    console.log(data)
+    data.message.forEach(img => {
+      imageContainer.innerHTML += `
+    <img src = '${img}'/>
+  `
+    });
+  })
+  function getDogBreed(filter) {
+
+  fetch(breedUrl).then(res => res.json()).then(data=> {
+    console.log (data)
+    Object.keys(data.message).forEach(breed => {
+      if (!filter || breed [0] === filter) {
+
+      dogBreeds.innerHTML += `
+        <li onclick = "this.style = 'color: red'"> 
+            ${breed}
+            <ul>
+              ${data.message[breed].map(subbreed => {
+                  return `
+                    <li>
+                      ${subbreed}
+                    </li>
+                  `
+              }).join('')}
+            </ul>
+        </li>
+      `
+    }
+    })
+  })
+}
+getDogBreed()
+  breedDropDown.addEventListener('change', () => {
+    const value = breedDropDown.value
+    console.log(value)
+    dogBreeds.innerHTML = ''
+    getDogBreed(value)
+  })
+})
